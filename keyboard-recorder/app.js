62adeb719877c49cdd3ea0bdf19cca3aceda1fce
Scott Metoyer <scott.metoyer@ucr.edu>
2019-11-19 08:01:54 -0800

    Added stdin receiving code

--- keyboard-recorder/app.js
+++ keyboard-recorder/app.js
@@ -1,2 +1,13 @@
 const readline = require('readline');
-s
+readline.emitKeypressEvents(process.stdin);
+
+const input = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+input.on('line', (input) => {
+    console.log('Received input: ' + input);
+
+    // Timestamp it and save it to whatever persistence mechanism we use
+});
