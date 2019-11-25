866f6107be83eb6b0fe65ecadba004d31d655d9d
Scott Metoyer <scott.metoyer@ucr.edu>
2019-11-25 08:52:24 -0800

    Completed first pass on file persistence

--- keyboard-recorder/app.js
+++ keyboard-recorder/app.js
@@ -1,13 +1,43 @@
 const readline = require('readline');
-readline.emitKeypressEvents(process.stdin);
+const fs = require('fs');
 
 const input = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-input.on('line', (input) => {
-    console.log('Received input: ' + input);
+// Create file for reading
+var now = new Date();
+var isoString = now.toISOString();
 
-    // Timestamp it and save it to whatever persistence mechanism we use
+var stream = fs.createWriteStream("Scans_" + isoString.replace(':', "-"), {
+    flags: 'a'
 });
+var start = Date.now();
+
+input.on('line', (input) => {
+    var millis = Date.now() - start;
+    console.log('Received input: ' + input);
+    stream.write(input + ", " + millis + "\n");
+});
+
+if (process.platform === "win32") {
+    var rl = require("readline").createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.on("SIGINT", function () {
+        shutdown();
+    });
+}
+
+process.on("SIGINT", function () {
+    shutdown();
+});
+
+function shutdown() {
+    console.log('Application exiting...');
+    stream.close();
+    process.exit();
+}
